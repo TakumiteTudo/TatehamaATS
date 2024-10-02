ad488e8e2d597b3e8b7db5e11276049712d52c52
TakumiteTudo <takumit2886@gmail.com>
2024-10-02 19:39:49 +0900

    忘れ去られた快急→急行変更適用 92年展示回送の回送-3対応

--- Diadata/DiaNameTrack.cs
+++ Diadata/DiaNameTrack.cs
@@ -15,6 +15,7 @@ namespace TatehamaATS.Diadata
                     TrainState.RouteDatabase.CircuitList[35].ChengeName("館浜下り場内1LB");
                     break;
 
+                //だんじり・試運転
                 case "回7281":
                 case "7281B":
                 case "回1195":
@@ -35,17 +36,18 @@ namespace TatehamaATS.Diadata
                 case "1280C":
                 case "回7190":
                 case "回7290":
-                    //元シナリオ：列番不定　館　浜　→発車共通／内容：発車２番振替 
+                    //元シナリオ：列番不定　館　浜　→発車共通／内容：発線２番振替 
                     TrainState.RouteDatabase.CircuitList[0].ChengeName("館浜下り場内1LB");
                     TrainState.RouteDatabase.CircuitList[1].ChengeName("館浜上り出発2R");
                     break;
 
+                //92年 4300展示回送用　
                 case "回7001":
-                    //92年 4300展示回送用　江ノ原検→館　浜　／内容：着線３番振替 
+                    //元シナリオ：回 607A　江ノ原検→館　浜　／内容：着線３番振替 
                     TrainState.RouteDatabase.CircuitList[34].ChengeName("館浜下り場内1LC");
                     break;
                 case "回7200":
-                    //92年 4300展示回送用　館　浜　→発車 ／内容：発車３番振替 
+                    //元シナリオ：回 862 　館　浜　→江ノ原検／内容：発線３番振替 
                     TrainState.RouteDatabase.CircuitList[0].ChengeName("館浜下り場内1LB");
                     TrainState.RouteDatabase.CircuitList[1].ChengeName("館浜上り出発3R");
                     break;

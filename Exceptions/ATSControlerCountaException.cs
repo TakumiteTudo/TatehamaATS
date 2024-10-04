ee9a8941a3301251281090ec1471cb870bf581af
TakumiteTudo <takumit2886@gmail.com>
2024-10-04 20:35:46 +0900

    ATS受け入れ用の故障作成

---
+++ Exceptions/ATSControlerCountaException.cs
@@ -0,0 +1,41 @@
+﻿namespace TatehamaATS.Exceptions
+{
+    /// <summary>
+    /// AE:ATS制御部カウンタ異常
+    /// </summary>
+    internal class ATSControlerCountaException : ATSCommonException
+    {
+        /// <summary>
+        /// AE:ATS制御部カウンタ異常
+        /// </summary>
+        public ATSControlerCountaException(int place) : base(place)
+        {
+        }
+        /// <summary>
+        /// AE:ATS制御部カウンタ異常
+        /// </summary>
+        public ATSControlerCountaException(int place, string message)
+            : base(place, message)
+        {
+        }
+        /// <summary>
+        /// AE:ATS制御部カウンタ異常
+        /// </summary>
+        public ATSControlerCountaException(int place, string message, Exception inner)
+            : base(place, message, inner)
+        {
+        }
+        public override string ToCode()
+        {
+            return Place.ToString() + "AE";
+        }
+        public override ResetConditions ResetCondition()
+        {
+            return ResetConditions.PowerReset;
+        }
+        public override OutputBrake ToBrake()
+        {
+            return OutputBrake.None;
+        }
+    }
+}
---
+++ Exceptions/ATSControlerException.cs
@@ -0,0 +1,41 @@
+﻿namespace TatehamaATS.Exceptions
+{
+    /// <summary>
+    /// AF:ATS制御部未定義故障
+    /// </summary>
+    internal class ATSControlerException : ATSCommonException
+    {
+        /// <summary>
+        /// AF:ATS制御部未定義故障
+        /// </summary>
+        public ATSControlerException(int place) : base(place)
+        {
+        }
+        /// <summary>
+        /// AF:ATS制御部未定義故障
+        /// </summary>
+        public ATSControlerException(int place, string message)
+            : base(place, message)
+        {
+        }
+        /// <summary>
+        /// AF:ATS制御部未定義故障
+        /// </summary>
+        public ATSControlerException(int place, string message, Exception inner)
+            : base(place, message, inner)
+        {
+        }
+        public override string ToCode()
+        {
+            return Place.ToString() + "AF";
+        }
+        public override ResetConditions ResetCondition()
+        {
+            return ResetConditions.PowerReset;
+        }
+        public override OutputBrake ToBrake()
+        {
+            return OutputBrake.None;
+        }
+    }
+}
